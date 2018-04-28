493f508153171420771c4488e47ddd074531ca12
Arnvvd <pinot.arnaud@gmail.com>
2018-04-28 16:11:57 +0200

    Change organisation tracker manager to prepare popup system

--- Assets/Scripts/Managers/TrackerManager.cs
+++ Assets/Scripts/Managers/TrackerManager.cs
@@ -9,37 +9,82 @@ public class TrackerManager : MonoBehaviour, ITrackableEventHandler
 	private GameObject scan;
 	private GameObject text;
 
-	protected virtual void Start()
-	{
+	protected virtual void Start() {
 		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		
+		// Trackable Behaviour
 		if (mTrackableBehaviour)
 			mTrackableBehaviour.RegisterTrackableEventHandler(this);
-		scan = GameObject.FindWithTag("scan");
-		text = GameObject.FindWithTag("text");
+		
 	}
 
-	public void OnTrackableStateChanged(
-		TrackableBehaviour.Status previousStatus,
-		TrackableBehaviour.Status newStatus)
-	{
+	
+	public void OnTrackableStateChanged( TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+		
 		if (newStatus == TrackableBehaviour.Status.DETECTED ||
 			newStatus == TrackableBehaviour.Status.TRACKED ||
-			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
-		{
-			Debug.Log("Custoooom Trackable " + mTrackableBehaviour.TrackableName + " found");
-			if (!GlobalManager.instance.isLoggin)
-			{	
-				AuthManager.Instance.OnLogin(mTrackableBehaviour.TrackableName);
-				scan.GetComponent<Image>().color = new Color32(0,0,0,100);
-				text.SetActive(false); 
-			}
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+			
+			Debug.Log("Custom Trackable " + mTrackableBehaviour.TrackableName + " found");
+			OnTrackingFound();
 		}
-		else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
-			newStatus == TrackableBehaviour.Status.NOT_FOUND)
-		{
-			scan.GetComponent<Image>().color = new Color32(255,255,255,100);
+		
+		else if (previousStatus == TrackableBehaviour.Status.TRACKED && newStatus == TrackableBehaviour.Status.NOT_FOUND) {
+			
 			Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+			onTrackingLost();
 		}
-
 	}
+	
+	
+	// Tracking Found
+	private void OnTrackingFound() {
+		
+		switch (mTrackableBehaviour.TrackableName) {
+			case "qrcode":
+				if (GlobalManager.instance.isLoggin) return;
+				onScan();
+				break;
+			
+			default:
+				Debug.Log("Tracking FOUND but no specific action");
+				break;
+		}		 
+	}
+	
+	
+	// Tracking Lost
+	private void onTrackingLost() {
+		switch (mTrackableBehaviour.TrackableName) {
+			case "qrcode":
+				offScan();
+				break;
+			
+			default:
+				Debug.Log("Tracking LOST but no specific action");
+				break;
+		}
+	}
+	
+	
+	///////////////
+	// Methods
+	///////////////
+	
+	// Scan
+	private void onScan() {
+		
+		scan = GameObject.FindWithTag("scan");
+		text = GameObject.FindWithTag("text");
+		
+		AuthManager.Instance.OnLogin(mTrackableBehaviour.TrackableName);
+		scan.GetComponent<Image>().color = new Color32(0,0,0,100);
+		text.SetActive(false);
+	}
+
+	private void offScan() {
+		scan.GetComponent<Image>().color = new Color32(255,255,255,100);
+	}
+	
+	
 }
