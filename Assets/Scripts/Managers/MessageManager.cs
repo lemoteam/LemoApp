eafccb4b6efd42ff68afcd4dde75e49591303f41
Arnvvd <pinot.arnaud@gmail.com>
2018-04-30 01:14:23 +0200

    Prepare animation

--- Assets/Scripts/Managers/MessageManager.cs
+++ Assets/Scripts/Managers/MessageManager.cs
@@ -1,18 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MessageManager : MonoBehaviour
 {
+    private static MessageManager instance;
+    
     private static GameObject popupPanel;
     private static Text popupText;
+    private static Animator popupAni;
+    private static bool isAnimated;
 
     private void Awake() {
+        instance = this;
         popupPanel = GameObject.Find("PopupPanel").GetComponent<GameObject>();
         popupText = GameObject.Find("PopupText").GetComponent<Text>();
+        popupAni = GameObject.Find("PopupPanel").GetComponent<Animator>();
     }
 
-    public static void ShowMessage(string key) {
-        var list = GlobalManager.instance.messageList;
+    public static void ShowMessage(string key) { 
+        var list = GlobalManager.instance.messageList; 
         
         foreach (var item in list) {
             if (item.key == key) {
@@ -22,6 +30,18 @@ public class MessageManager : MonoBehaviour
     }
 
     private static void ActivePopup(Message item) {
+        if (isAnimated) return;
+        isAnimated = true;
+        instance.StartCoroutine(DisplayPopup(item));
+    }
+
+    private static IEnumerator DisplayPopup(Message item) {
+        
         popupText.text = item.value;
+        popupAni.Play("popupIn");
+        yield return new WaitForSeconds(4);
+        popupText.text = "back";
+        popupAni.Play("popupOut");
+        isAnimated = false;
     }
 }
