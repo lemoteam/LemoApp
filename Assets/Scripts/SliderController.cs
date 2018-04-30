f0b5fb7c77a985020c10ee4594a3a293e818a521
johnfordman <ndoulou.jean@gmail.com>
2018-04-30 02:57:36 +0200

    sliderController debut

---
+++ Assets/Scripts/SliderController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SliderController : MonoBehaviour {
+
+    private Transform max; 
+    // private float distance;
+    public ReaderManager readerManager;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log ("START !!!!!!");
+        max = GameObject.Find ("Max").transform;
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        Vector3 distance = max.position - this.transform.position;
+        Debug.Log("position max :"+max);
+        Debug.Log("position image target:"+this.transform.position);
+        //Debug.Log("distance : "+ distance.magnitude);
+        // readerManager.UpdateReaderSettings(parameter);
+    }
+}
