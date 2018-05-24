78710554e014636ecb920323a031ca2da7bc4a51
jndoulou <jndoulou@bigyouth.com>
2018-05-24 15:02:07 +0200

    slider choice 3

---
+++ Assets/Scripts/Choic3SliderController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+
+public class Choic3SliderController : MonoBehaviour {
+    private GameObject indicator;
+    private GameObject[] indexes;
+    private float previousIndex = -1;
+    private float min;
+	
+    // Use this for initialization
+    void Start () {
+        indicator = GameObject.FindWithTag("indicator");
+        indexes = GameObject.FindGameObjectsWithTag("index");
+        min =  Vector3.Distance(indexes[0].transform.position, indicator.transform.position);
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        if (indicator.GetComponent<MeshRenderer>().enabled)
+        {
+            getMinDistance();
+        }
+    }
+    
+    private void getMinDistance()
+    {
+        // var min = Vector3.Distance(indexes[0].transform.position, indicator.transform.position);
+        for (int i = 1; i < indexes.Length; i++)
+        {
+            var distance = Vector3.Distance(indexes[i].transform.position, indicator.transform.position);
+            if (distance < min)
+            {
+                min = distance;
+                var currentIndex = i;
+                if (previousIndex != currentIndex)
+                {
+                    GlobalManager.instance.dynamicHasChanded = true;
+                    Debug.Log(indexes[i].name + " index est : " + i);
+                    var dynamicValue = Single.Parse(indexes[i].name);
+                    transform.localScale = new Vector3(transform.localScale.x, dynamicValue, transform.localScale.z);
+                    var prop = GlobalManager.instance.reader.GetType().GetProperty("dynamic");
+                    if (prop != null) prop.SetValue(GlobalManager.instance.reader, dynamicValue, null);
+                    previousIndex = currentIndex;
+                }
+            }
+        }
+    }
+}
