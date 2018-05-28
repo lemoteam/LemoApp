f1427cc31792483bf23519066d5c3101ea26f903
johnfordman <ndoulou.jean@gmail.com>
2018-05-28 04:32:53 +0200

    first scene with falling particles and add change noise script

---
+++ Assets/ChangeNoise.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ChangeNoise : MonoBehaviour
+{
+    private ReaderManager readerManager;
+    public float noiseFrequency;
+    // public float noiseStrentgh;
+   
+    // Use this for initialization
+    void Start ()
+    {
+        var dynamicValue = float.Parse(readerManager.GetReaderSetting("dynamic"));
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var no = ps.noise;
+        no.enabled = true;
+        noiseFrequency = dynamicValue;
+        //no.strength = 1.0f;
+        no.quality = ParticleSystemNoiseQuality.High;
+        no.frequency = noiseFrequency;
+    }
+	
+}
