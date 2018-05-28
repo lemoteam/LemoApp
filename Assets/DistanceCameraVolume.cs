43813afc657063488d8a9b85a11ab74c9db17999
johnfordman <ndoulou.jean@gmail.com>
2018-05-28 02:27:24 +0200

    sound volume on distance camera scene 1

---
+++ Assets/DistanceCameraVolume.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class DistanceCameraVolume : MonoBehaviour {
+
+	private GameObject arCamera; 
+	private AudioSource soundTarget;
+	private GameObject sceneMarker;
+	private GameObject tree;
+
+	public float Map(float from, float to, float from2, float to2, float value) {
+		if(value <= from2) {
+			return from;
+		} else if(value >= to2) {
+			return to;
+		} else {
+			return (to - from) * ((value - from2) / (to2 - from2)) + from;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		sceneMarker = GameObject.FindWithTag("sceneMarker");
+		soundTarget = sceneMarker.GetComponent<AudioSource>();
+		tree = GameObject.FindWithTag("tree");
+		arCamera = GameObject.Find ("ARCamera"); 
+		Debug.Log ("camera : " + arCamera);
+		soundTarget.loop = true;
+		Debug.Log ("audio volume : " + soundTarget.volume);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (tree)
+		{
+			Vector3 distance = arCamera.transform.position - tree.transform.position;
+			float Len = (float) distance.magnitude;
+			// flot volume = Debug.Log("distance map : " + Map(0.050f,1.0f,0.05f,2000.0f,Len));	
+			float volumeMap = Map(1.0f,0.02f,900.0f,1200.0f,Len);
+			soundTarget.volume = volumeMap;
+		}
+	
+	}
+}
---
+++ Assets/Scripts/ToggleSound.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ToggleSound : MonoBehaviour,
+	ITrackableEventHandler
+{
+
+	#region PRIVATE_MEMBER_VARIABLES
+
+	private TrackableBehaviour mTrackableBehaviour;
+	private AudioSource audioSource;
+
+	#endregion // PRIVATE_MEMBER_VARIABLES
+
+	#region UNTIY_MONOBEHAVIOUR_METHODS
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	#endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+	#region PUBLIC_METHODS
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+		    newStatus == TrackableBehaviour.Status.TRACKED)
+		{
+			OnTrackingFound();
+		}
+		else
+		{
+			OnTrackingLost();
+		}
+	}
+
+	#endregion // PUBLIC_METHODS
+
+	#region PRIVATE_METHODS
+
+
+	private void OnTrackingLost()
+	{
+		audioSource.Stop();	
+		Debug.Log("ciaaaao");
+	}
+	
+	private void OnTrackingFound()
+	{
+		audioSource.Play();	
+		Debug.Log("salut");
+	}
+			
+	#endregion // PRIVATE_METHODS
+}    
+
+
