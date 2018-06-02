d17feadb3e3836330c54f625e0e6497819990707
Arnvvd <pinot.arnaud@gmail.com>
2018-06-02 18:28:34 +0200

    Look at between emmi and catiche

---
+++ Assets/Scripts/Utilities/LookAt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+    
+	public Transform target;
+
+	void Update()
+	{
+		var lookPos = target.position - transform.position;
+		lookPos.y = 0;
+		var rotation = Quaternion.LookRotation(lookPos);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * .1f);
+	}
+}
