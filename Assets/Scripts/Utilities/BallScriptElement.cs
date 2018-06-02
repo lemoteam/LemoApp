3a74d0cbb011be6096e23e6927ebf198828f7749
Arnvvd <pinot.arnaud@gmail.com>
2018-06-02 23:35:54 +0200

    Prepare aquarium with ball inclinaison

---
+++ Assets/Scripts/Utilities/BallScriptElement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScriptElement : MonoBehaviour
+{
+
+	public GameObject plane;
+	public GameObject spawnPoint;
+	private Rigidbody elRigidbody;
+	private Vector3 NewGravitySettings; 
+
+	// Use this for initialization
+	void Start ()
+	{
+		elRigidbody = transform.GetComponent<Rigidbody>();
+		NewGravitySettings = new Vector3(0f, -100f, 0f); // Physics.gravity
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate()
+	{
+		elRigidbody.AddForce(NewGravitySettings * elRigidbody.mass);
+	}
+}
