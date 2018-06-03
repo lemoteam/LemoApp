42b633041dfbd61ced6d7ea46a452c51c2125f2f
Arnvvd <pinot.arnaud@gmail.com>
2018-06-03 03:40:56 +0200

    Respawn when lost marker

--- Assets/Scripts/Utilities/BallScriptElement.cs
+++ Assets/Scripts/Utilities/BallScriptElement.cs
@@ -1,22 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class BallScriptElement : MonoBehaviour
+
+public class BallScriptElement :  MonoBehaviour, ITrackableEventHandler
 {
-
-	public GameObject plane;
-	public GameObject spawnPoint;
 	private Rigidbody elRigidbody;
 	private Vector3 NewGravitySettings;
 	private bool isReady = false;
+	private GameObject character;
+	private GameObject spawn;
+	private Vector3 position;
+	
+	private TrackableBehaviour mTrackableBehaviour;
 
-	// Use this for initialization
-	void Start ()
-	{
-		elRigidbody = transform.GetComponent<Rigidbody>();
+	void Start() {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		
+		character = GameObject.FindGameObjectWithTag("character");
+		spawn = GameObject.FindGameObjectWithTag("characterSpawn");
+		elRigidbody = character.GetComponent<Rigidbody>();
 		elRigidbody.isKinematic = true;
-		StartCoroutine(addGravity());
+		
+		// Trackable Behaviour
+		if (mTrackableBehaviour)
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+	}
+
+	
+	public void OnTrackableStateChanged( TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+		
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+		    newStatus == TrackableBehaviour.Status.TRACKED ||
+		    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+			
+			Debug.Log("Custom Trackable " + mTrackableBehaviour.TrackableName + " found");
+			OnTrackingFound();
+		}
+		
+		else if (previousStatus == TrackableBehaviour.Status.TRACKED && newStatus == TrackableBehaviour.Status.NO_POSE) {
+			
+			Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+			OnTrackingLost();
+		}
 	}
 	
 
@@ -25,6 +52,16 @@ public class BallScriptElement : MonoBehaviour
 		yield return new WaitForSeconds(1.5f);
 		elRigidbody.isKinematic = false;
 		NewGravitySettings = new Vector3(0f, -30f, 0f); // Physics.gravity
-		isReady = true;
+	}
+	
+	// Tracking Found
+	private void OnTrackingFound() {
+		StartCoroutine(addGravity());
+	}
+	
+	// Tracking Lost
+	private void OnTrackingLost() {
+		elRigidbody.isKinematic = true;
+		character.transform.position = spawn.transform.position;
 	}
 }
