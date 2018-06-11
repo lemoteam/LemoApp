810525cd6629e0ea7253e05eebe3e073f288e560
johnfordman <ndoulou.jean@gmail.com>
2018-06-11 04:03:21 +0200

    healing choice 1

--- Assets/Scripts/GetHealingColor.cs
+++ Assets/Scripts/GetHealingColor.cs
@@ -27,11 +27,11 @@ public class GetHealingColor : MonoBehaviour {
 				break;
 			case "2":
 				// moodName = "extraordinaire";
-				getColor(new Color(1,0.06666667f,0.5333334f), new Color(1,0.7058824f,0.1058824f));
+				getColor(new Color(0.7264151f,0.119927f,0.423171f), new Color(1,0.7058824f,0.1058824f));
 				break;
 			case "3":
 				// moodName = "mysterieux";
-				getColor(new Color(0.05882353f,0.1019608f,0.2941177f),new Color(0.5490196f,0.09411765f,0.4862745f));
+				getColor(new Color(0.09242612f,0.1019608f,0.2941177f),new Color(0.5490196f,0.09411765f,0.4862745f));
 				break;
 		}
 	}
